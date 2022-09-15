783634dfa1ff14a1a130208581d0bced0d82f9ec
git-hub0101 <kravikumars444@gmail.com>
2022-09-15 00:20:21 -0400

    changed publish url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='karpiworld@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/karpigit/ravi-publish-pypi',
     keywords='ravi',
     install_requires=[
       ],
