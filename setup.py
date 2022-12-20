3738865a9bb6da5b9be527a67e9797252d64680c
git-hub0101 <kravikumars444@gmail.com>
2022-12-20 12:25:47 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tamil',
-    version='0.1',
+    version='0.2',
     license='MIT',
     author="Karpi",
     author_email='karpiworld@gmail.com',
